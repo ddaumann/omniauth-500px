b323c548cda49f2a0a85330e10688ca410643107
Dirk Daumann <dirk.daumann@mac.com>
2013-04-10 10:54:40 +0200

    changed option name

--- lib/omniauth/strategies/500px.rb
+++ lib/omniauth/strategies/500px.rb
@@ -6,7 +6,7 @@ module OmniAuth
 
     class FiveHundredPx < OmniAuth::Strategies::OAuth
       
-      option :name, '500px'
+      option :name, 'FiveHundredPx'
       
       option :client_options, {
         :access_token_path => "/v1/oauth/access_token",
